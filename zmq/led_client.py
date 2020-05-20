4d7eba3bdd8106d5c90b75df1f25ed7ca06d5bac
bdebruijn <bdebruijn@luminize.nl>
2020-05-20 22:01:42 +0200

    led_client.py: add commands to send to server

--- zmq/led_client.py
+++ zmq/led_client.py
@@ -5,19 +5,46 @@
 #
 
 import zmq
+import time
+import argparse
 
-context = zmq.Context()
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-c", "--command", required = True)
+ap.add_argument("-n", "--hostname", required = True)
+args = vars(ap.parse_args())
 
-#  Socket to talk to server
-print("Connecting to hello world server…")
-socket = context.socket(zmq.REQ)
-socket.connect("tcp://localhost:5555")
+# dict of commands for sanity checking
+cmds = {
+    "color_r": "color_r",
+    "color_g": "color_g",
+    "color_b": "color_b",
+    "color_y": "color_y",
+    "color_c": "color_c",
+    "color_p": "color_p",
+    "color_w": "color_w",
+    "color_o": "color_o",
+    "br_100": "br_100",
+    "br_50": "br_50",
+    "br_10": "br_10",
+    "br_0": "br_0"
+}
 
-#  Do 10 requests, waiting each time for a response
-for request in range(10):
-    print("Sending request %s …" % request)
-    socket.send(b"Hello")
+try:
+    cmd = cmds[args["command"]]
+    context = zmq.Context()
 
-    #  Get the reply.
-    message = socket.recv()
-    print("Received reply %s [ %s ]" % (request, message))
+    # Socket to talk to server
+    print("Connecting to Neopixel LED server…")
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://{}:5555".format(args["hostname"]))
+
+    # Send the command
+    socket.send_string(cmd)
+
+    # Get the reply.
+    message = socket.recv_string()
+    print("Received reply: [ %s ]" % message)
+
+except KeyError:
+    print("unknown command: %s" % args["command"])
